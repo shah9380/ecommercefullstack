b9a9d20912f4b9ed180f46594ff6ea06d4c3b197
shah9380 <130676464+shah9380@users.noreply.github.com>
2024-03-24 15:30:29 +0530

    user is created and as well as added a new middle ware for error handling

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,25 +1,24 @@
+const expressAsyncHandler = require('express-async-handler');
 const User = require('../model/userModel');
 
 
-
-const createUser = async (req, res)=>{
-    //getting the user typed mail
-    const email = req.body.email;
-
-    //finding a user in data base
-    const findUser = await User.findOne({email: email});
-
-    if(!findUser){
-        //creating a new user if user doesn't exist
-        const newUser = await User.create(req.body);
-        res.json(newUser);
-    }else{
-        //sending back the message that user already exists
-        res.json({
-            message: "User Already Exists",
-            success: false
-        })
+const createUser = expressAsyncHandler(
+    async (req, res)=>{
+        //getting the user typed mail
+        const email = req.body.email;
+    
+        //finding a user in data base
+        const findUser = await User.findOne({email: email});
+    
+        if(!findUser){
+            //creating a new user if user doesn't exist
+            const newUser = await User.create(req.body);
+            res.json(newUser);
+        }else{
+            //sending back the message that user already exists
+            throw new Error('User Already Exists');
+        }
     }
-}
-
+    
+)
 module.exports = { createUser };
--- index.js
+++ index.js
@@ -7,13 +7,18 @@ const PORT = process.env.PORT || 4000;
 
 const authRouter = require('./routes/authRoutes');
 const bodyParser = require('body-parser');
+const { notFound, errorHandler } = require('./middlewares/errorHandler');
 
 //connecting to Database
 dbConnect();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
+//calling an api using authRouter for a new user
 app.use('/api/user', authRouter)
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, ()=>{
     console.log(`server is running at port ${PORT}`);
