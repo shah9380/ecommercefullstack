4c0ebaafd4b3d1f6abf4fe67dfe413c82f8f0313
shah9380 <130676464+shah9380@users.noreply.github.com>
2024-05-04 18:07:16 +0530

    some changes in userModel

--- model/userModel.js
+++ model/userModel.js
@@ -64,6 +64,7 @@ userSchema.methods.isPasswordMatched = async function(enteredPassword){
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
+
 userSchema.methods.createPasswordResetToken = async function(){
     const resetToken = crypto.randomBytes(32).toString("hex");
     this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
